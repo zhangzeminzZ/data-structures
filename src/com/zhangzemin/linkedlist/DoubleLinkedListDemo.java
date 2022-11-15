abecf23cb5fd943db9982fec26b8a00cc11a8529
zhangzemin <zhangzeminzz@163.com>
2022-11-15 20:55:52 +0800

    双向链表

---
+++ src/com/zhangzemin/linkedlist/DoubleLinkedListDemo.java
@@ -0,0 +1,138 @@
+package com.zhangzemin.linkedlist;
+
+public class DoubleLinkedListDemo {
+    public static void main(String[] args) {
+        // 创建节点
+        HeroNode2 hero1 = new HeroNode2(1, "宋江", "及时雨");
+        HeroNode2 hero2 = new HeroNode2(2, "卢俊义", "玉麒麟");
+        HeroNode2 hero3 = new HeroNode2(3, "吴用", "智多星");
+        HeroNode2 hero4 = new HeroNode2(4, "林冲", "豹子头");
+        // 创建一个双向链表
+        DoubleLinkedList doubleLinkedList = new DoubleLinkedList();
+        doubleLinkedList.add(hero1);
+        doubleLinkedList.add(hero2);
+        doubleLinkedList.add(hero3);
+        doubleLinkedList.add(hero4);
+
+        doubleLinkedList.list();
+
+        // 修改
+        HeroNode2 newHeroNode = new HeroNode2(4, "公孙胜", "入云龙");
+        doubleLinkedList.update(newHeroNode);
+        System.out.println("修改后...");
+        doubleLinkedList.list();
+
+        // 删除
+        doubleLinkedList.del(3);
+        System.out.println("删除后...");
+        doubleLinkedList.list();
+    }
+}
+
+class DoubleLinkedList {
+
+    private HeroNode2 head = new HeroNode2(0, "", "");
+
+    public HeroNode2 getHead() {
+        return head;
+    }
+
+    public void list() {
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode2 temp = head.next;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            System.out.println(temp);
+            temp = temp.next;
+        }
+    }
+
+    public void add(HeroNode2 heroNode) {
+        HeroNode2 temp = head;
+        while (true) {
+            if (temp.next == null) {
+                temp.next = heroNode;
+                heroNode.pre = temp;
+                break;
+            }
+            temp = temp.next;
+        }
+    }
+
+    public void update(HeroNode2 newHeroNode) {
+        if (head.next == null) {
+            System.out.println("链表为空~");
+            return;
+        }
+        HeroNode2 temp = head.next;
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == newHeroNode.no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.name = newHeroNode.name;
+            temp.nickname = newHeroNode.nickname;
+        } else {
+            System.out.printf("没有找到 编号 %d 的节点，不能修改\n", newHeroNode.no);
+        }
+    }
+
+    public void del(int no) {
+        if (head.next == null) {
+            System.out.println("链表为空");
+            return;
+        }
+        HeroNode2 temp = head.next;
+        boolean flag = false;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            if (temp.no == no) {
+                flag = true;
+                break;
+            }
+            temp = temp.next;
+        }
+        if (flag) {
+            temp.pre.next = temp.next;
+            if (temp.next != null) {
+                temp.next.pre = temp.pre;
+            }
+        } else {
+            System.out.printf("要删除的 %d 节点不存在\n", no);
+        }
+    }
+}
+
+class HeroNode2 {
+    public int no;
+    public String name;
+    public String nickname;
+    public HeroNode2 next;
+    public HeroNode2 pre;
+
+    public HeroNode2(int no, String name, String nickname) {
+        this.no = no;
+        this.name = name;
+        this.nickname = nickname;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode [no=" + no + ", name=" + name + ", nickname=" + nickname + "]";
+    }
+
+}
